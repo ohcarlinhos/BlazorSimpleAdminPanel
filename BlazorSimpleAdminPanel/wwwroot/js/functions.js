fd33cabd758249cdd1b29cf5a0b55624d30c90cc
Carlos Júnior <crpu4a@gmail.com>
2024-08-04 00:19:14 -0300

    salvando

---
+++ BlazorSimpleAdminPanel/wwwroot/js/functions.js
@@ -0,0 +1,12 @@
+﻿window.log = (value) => {
+    console.log(value)
+}
+
+window.callDotNetFunction = (value) => normalMethod(value)
+
+function normalMethod(value) {
+    DotNet.invokeMethodAsync("BlazorSimpleAdminPanel", "MethodCallFromJs", value).then((response) => {
+        console.log(response);
+    })
+}
+
